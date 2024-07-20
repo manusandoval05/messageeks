294c75ae9853ad4c25288413c8005dc159875a54
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-07-20 08:41:19 -0600

    implement general auth layouts and path protection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -67,12 +67,12 @@ const authGuard: Handle = async ({ event, resolve }) => {
   event.locals.session = session
   event.locals.user = user
 
-  if (!event.locals.session && event.url.pathname.startsWith('/private')) {
-    redirect(303, '/auth')
+  if (!event.locals.session && event.url.pathname.startsWith('/@me')) {
+    redirect(303, '/auth/login')
   }
 
   if (event.locals.session && event.url.pathname === '/auth') {
-    redirect(303, '/private')
+    redirect(303, '/@me')
   }
 
   return resolve(event)
