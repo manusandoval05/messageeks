b9ae71f02ff50b511015f2f8a1c1ce53430ed5ad
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-07-20 08:08:04 -0600

    implement more reusable auth layout

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -12,5 +12,14 @@
 
 <!-- App Shell -->
 <AppShell>
-    <slot />
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=red&shade=600" alt="Your Company">
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight ">Sign in to your account</h2>
+        </div>
+        
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <slot/>
+        </div>
+    </div>
 </AppShell>
