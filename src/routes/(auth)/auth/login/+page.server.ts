de2ac5a61c2dd9a5ff4c43676e0ae680df6a0ca5
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-07-20 08:49:11 -0600

    implement initial login logic

---
+++ src/routes/(auth)/auth/login/+page.server.ts
@@ -0,0 +1,19 @@
+import { redirect } from '@sveltejs/kit';
+
+export const actions = {
+    login: async({ request, locals: { supabase }}) => {
+        const formData = await request.formData();
+
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+
+        const { error } = await supabase.auth.signInWithPassword({ email, password }); 
+
+        if(error){
+            console.log(error); 
+            redirect(303, "/auth/error");
+        }
+
+        redirect(303, "/@me");
+    }
+}
