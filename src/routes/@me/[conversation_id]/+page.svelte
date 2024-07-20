fc82bb2f6d440a66dd533c15b81f8945e2244334
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-07-20 07:37:43 -0600

    redesign chat bubbles with left alignment

--- src/routes/@me/[conversation_id]/+page.svelte
+++ src/routes/@me/[conversation_id]/+page.svelte
@@ -115,7 +115,8 @@
                     </div>
                 </div>
             {:else}
-                <div class="grid grid-cols-[1fr_auto] gap-2">
+                <div class="grid grid-cols-[auto_1fr] gap-2">
+					<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
                     <div class="card p-4 rounded-tr-none space-y-2 {bubble.color}">
                         <header class="flex justify-between items-center">
                             <p class="font-bold">{bubble.name}</p>
@@ -123,7 +124,6 @@
                         </header>
                         <p>{bubble.message}</p>
                     </div>
-                    <Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
                 </div>
             {/if}
         {/each}
