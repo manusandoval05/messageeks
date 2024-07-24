cc22d79a54c5211f0c1b6bfe321cc0c959db6bc1
thrinkler <aeshadow0935@gmail.com>
2024-07-23 22:53:24 -0600

    add 'secure: false' to cookies created by supabase

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -20,7 +20,7 @@ const supabase: Handle = async ({ event, resolve }) => {
 			 */
 			setAll: (cookiesToSet) => {
 				cookiesToSet.forEach(({ name, value, options }) => {
-					event.cookies.set(name, value, { ...options, path: '/' });
+					event.cookies.set(name, value, { ...options, path: '/', secure: false });
 				});
 			}
 		}
