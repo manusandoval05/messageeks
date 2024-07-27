737413ca790d8366b0905d02f4809c2a5075a3e2
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-07-27 08:55:11 -0600

    prettify login page svelte

--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -1,33 +1,30 @@
 <script>
 	export let data;
-    import {Auth} from '@supabase/auth-ui-svelte';
-    import {ThemeSupa} from '@supabase/auth-ui-shared'
-    import {goto} from '$app/navigation'
-    let {supabase, session} = data
+	import { Auth } from '@supabase/auth-ui-svelte';
+	import { ThemeSupa } from '@supabase/auth-ui-shared';
+	import { goto } from '$app/navigation';
+	let { supabase, session } = data;
 
-    $: ({supabase, session} = data)
-    
-    $:if(session){
-        console.log("onsession")
-        goto("/channels/@me")
-    }
-    
+	$: ({ supabase, session } = data);
+
+	$: if (session) {
+		console.log('onsession');
+		goto('/channels/@me');
+	}
 </script>
+
 <form class="space-y-6" action="?/login" method="POST">
-    <div class="flex flex-col">
-        <p>Create an account or login</p>
-        <Auth
-            supabaseClient={supabase}
-            theme="dark"
-            appearance={{
-                theme:ThemeSupa,
-                style:{
-                    input: "width: 400px;"
-                }
-            }
-                
-            }
-        
-            />
-    </div>
+	<div class="flex flex-col">
+		<p>Create an account or login</p>
+		<Auth
+			supabaseClient={supabase}
+			theme="dark"
+			appearance={{
+				theme: ThemeSupa,
+				style: {
+					input: 'width: 400px;'
+				}
+			}}
+		/>
+	</div>
 </form>
