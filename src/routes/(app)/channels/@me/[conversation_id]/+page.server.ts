76ae0d0fa0217665c8fc920373120030f1681369
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-07 21:40:32 -0600

    implement ordering inside of the query

--- src/routes/(app)/channels/@me/[conversation_id]/+page.server.ts
+++ src/routes/(app)/channels/@me/[conversation_id]/+page.server.ts
@@ -1,14 +1,14 @@
 import { error } from '@sveltejs/kit';
 
-export const load = async ({ params, locals: { supabase, safeGetSession }}) => {
-    
-    const { session, user } = await safeGetSession();
+export const load = async ({ params, locals: { supabase, safeGetSession } }) => {
+	const { user } = await safeGetSession();
 
-    if(!user) return;
+	if (!user) return;
 
-    const conversationMessagesRequest = await supabase
-        .from("conversations")
-        .select(`
+	const conversationMessagesRequest = await supabase
+		.from('conversations')
+		.select(
+			`
             accepted_by_receiver,
             sender_username,
             receiver_username,
@@ -18,20 +18,23 @@ export const load = async ({ params, locals: { supabase, safeGetSession }}) => {
                 sender_id,
                 content
             )
-        `)
-        .eq("id", Number(params.conversation_id));
+        `
+		)
+		.eq('id', Number(params.conversation_id))
+		.order('id', {
+			ascending: true
+		});
 
-    
-    if(conversationMessagesRequest.error){
-        console.log(conversationMessagesRequest.error);
-        throw error(400, "Failed to load conversation");
-    };
-    
-    return {
-        conversation_messages: conversationMessagesRequest.data[0].conversation_messages,
-        accepted_by_receiver: conversationMessagesRequest.data[0].accepted_by_receiver,
-        sender_username: conversationMessagesRequest.data[0].sender_username,
-        receiver_username: conversationMessagesRequest.data[0].receiver_username, 
-        conversation_id: Number(params.conversation_id),
-    }
+	if (conversationMessagesRequest.error) {
+		console.log(conversationMessagesRequest.error);
+		throw error(400, 'Failed to load conversation');
+	}
+
+	return {
+		conversation_messages: conversationMessagesRequest.data[0].conversation_messages,
+		accepted_by_receiver: conversationMessagesRequest.data[0].accepted_by_receiver,
+		sender_username: conversationMessagesRequest.data[0].sender_username,
+		receiver_username: conversationMessagesRequest.data[0].receiver_username,
+		conversation_id: Number(params.conversation_id)
+	};
 };
