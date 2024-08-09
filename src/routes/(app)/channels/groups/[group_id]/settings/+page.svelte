b43ebfe2c9e39e91233c08aeb0c0e12efe41dd7f
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-08 22:59:37 -0600

    add copy link to invite and change redirect bug

---
+++ src/routes/(app)/channels/groups/[group_id]/settings/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+	import { clipboard } from '@skeletonlabs/skeleton';
+
+	export let data;
+
+	const inviteGroupUrl = `${data.origin_url}/invite/group/${data.group_invite_id}`;
+</script>
+
+<Accordion>
+	<AccordionItem open>
+		<svelte:fragment slot="lead">
+			<span class="material-symbols-outlined"> share </span>
+		</svelte:fragment>
+		<svelte:fragment slot="summary">Enlace de invitación</svelte:fragment>
+		<svelte:fragment slot="content">
+			<div class="flex gap-2">
+				<p class="pt-2">{inviteGroupUrl}</p>
+				<button class="btn variant-filled" use:clipboard={inviteGroupUrl}>Copiar</button>
+			</div>
+		</svelte:fragment>
+	</AccordionItem>
+	<!-- ... -->
+</Accordion>
--- src/routes/invite/group/[invite_id]/+page.server.ts
+++ src/routes/invite/group/[invite_id]/+page.server.ts
@@ -2,42 +2,38 @@ import { error, redirect } from '@sveltejs/kit';
 import { createClient } from '@supabase/supabase-js';
 import { PRIVATE_SUPABASE_SERVICE_KEY } from '$env/static/private';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
-export const load = async({ params, locals: {user}}) => {
-    if(!user) throw redirect(300, "/");
+export const load = async ({ params, locals: { user } }) => {
+	if (!user) throw redirect(300, '/');
 
-    const supabaseSuperUser = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_KEY);
+	const supabaseSuperUser = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_KEY);
 
-    const groupInviteRequest =  await supabaseSuperUser
-        .from("group_invites")
-        .select()
-        .eq("id", params.invite_id);
+	const groupInviteRequest = await supabaseSuperUser
+		.from('group_invites')
+		.select()
+		.eq('id', params.invite_id);
 
-     
-    if(groupInviteRequest.error) throw error(400, "Couldn't process the invite");
+	if (groupInviteRequest.error) throw error(400, "Couldn't process the invite");
 
-    if(!groupInviteRequest.data.length) throw error(404, "Couldn't find the invitation");
+	if (!groupInviteRequest.data.length) throw error(404, "Couldn't find the invitation");
 
-    const groupID = groupInviteRequest.data[0].group_id;
+	const groupID = groupInviteRequest.data[0].group_id;
 
-    const groupMembersRequest = await supabaseSuperUser
-        .from("group_members")
-        .select("user_id")
-        .eq("user_id", user.id)
-        .eq("group_id", groupID);
+	const groupMembersRequest = await supabaseSuperUser
+		.from('group_members')
+		.select('user_id')
+		.eq('user_id', user.id)
+		.eq('group_id', groupID);
 
-    if(groupMembersRequest.error) throw error(400, "Couldn't process the invite");
+	if (groupMembersRequest.error) throw error(400, "Couldn't process the invite");
 
-    if(!groupMembersRequest.data.length){
+	if (!groupMembersRequest.data.length) {
+		const addGroupMemberRequest = await supabaseSuperUser.from('group_members').insert({
+			group_id: groupInviteRequest.data[0].group_id,
+			user_id: user.id
+		});
 
-        const addGroupMemberRequest = await supabaseSuperUser
-        .from("group_members")
-        .insert({
-            group_id: groupInviteRequest.data[0].group_id,
-            user_id: user.id
-        });
+		if (addGroupMemberRequest.error) throw error(400, "Couldn't process the invite");
+	}
 
-        if(addGroupMemberRequest.error) throw error(400, "Couldn't process the invite");
-    }
-
-    throw redirect(300, `/channels/groups/${groupInviteRequest.data[0].group_id}`);     
-}
+	throw redirect(303, `/channels/groups/${groupInviteRequest.data[0].group_id}`);
+};
