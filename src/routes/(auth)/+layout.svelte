ef823126b6b4461ff0f0f9a901e686419290f2d6
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-08 23:35:06 -0600

    remove align-center for small devices in log in screen

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -16,7 +16,7 @@
 </svelte:head>
 <!-- App Shell -->
 <AppShell>
-	<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+	<div class="flex min-h-full flex-col md:justify-center px-6 py-12 lg:px-8">
 		<div class="sm:mx-auto sm:w-full sm:max-w-sm">
 			<img
 				class="mx-auto h-10 w-auto"
