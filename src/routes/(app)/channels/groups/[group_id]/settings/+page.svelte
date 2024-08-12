fc8b5042054514f7b1efdebb9c9738ce4645f6bc
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-11 18:03:28 -0600

    add group member list

--- src/routes/(app)/channels/groups/[group_id]/settings/+page.svelte
+++ src/routes/(app)/channels/groups/[group_id]/settings/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
-	import { clipboard } from '@skeletonlabs/skeleton';
+	import { Accordion, AccordionItem, Avatar, popup, clipboard } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
 
 	export let data;
+	$: ({ supabase, group_id } = data);
 
 	const inviteGroupUrl = `${data.origin_url}/invite/group/${data.group_invite_id}`;
+
+	const popupGroupMember: PopupSettings = {
+		// Represents the type of event that opens/closed the popup
+		event: 'click',
+		// Matches the data-popup value on your popup element
+		target: 'popupGroupMember',
+		// Defines which side of your trigger the popup will appear
+		placement: 'bottom'
+	};
 </script>
 
 <div class="max-w-screen-lg">
@@ -28,10 +38,34 @@
 			<svelte:fragment slot="summary">Integrantes del grupo</svelte:fragment>
 			<svelte:fragment slot="content">
 				<ul class="list">
-					<li>
-						<span>(icon)</span>
-						<span class="flex-auto">Skeleton</span>
-					</li>
+					{#await supabase
+						.from('group_members')
+						.select(`
+							id,
+							group_id, 
+							user_id, 
+							user_profiles(display_name)
+						`)
+						.eq('group_id', group_id) then response}
+						{#each response.data ?? [] as group_member}
+							<li>
+								<Avatar initials="TU" width={'w-10'} />
+								<span class="flex-auto">
+									{group_member.user_profiles.display_name}
+								</span>
+								<button use:popup={popupGroupMember}>⋮</button>
+							</li>
+							<div class="card p-4 w-72 shadow-xl z-10" data-popup="popupGroupMember">
+								<div class="flex flex-col gap-2">
+									<button type="button" class="btn variant-filled-surface">
+										<span class="material-symbols-outlined"> person_remove </span>
+										<span>Quitar del grupo</span>
+									</button>
+								</div>
+								<div class="arrow bg-surface-700" />
+							</div>
+						{/each}
+					{/await}
 					<!-- ... -->
 				</ul>
 			</svelte:fragment>
