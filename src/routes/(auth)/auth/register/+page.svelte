5130141a3e6dcbe132197b28e2c3964a1778577d
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-13 02:07:41 -0600

    change text in button in register main action

--- src/routes/(auth)/auth/register/+page.svelte
+++ src/routes/(auth)/auth/register/+page.svelte
@@ -86,7 +86,7 @@
 				<button
 					type="submit"
 					class="flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-					>Iniciar sesión</button
+					>Crear cuenta</button
 				>
 			</div>
 		</form>
