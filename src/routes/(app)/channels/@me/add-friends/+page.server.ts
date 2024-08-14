0e90262b0b07ffe259b7304c7780e41710a25891
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-13 20:25:00 -0600

    fix invite links

--- src/routes/(app)/channels/@me/add-friends/+page.server.ts
+++ src/routes/(app)/channels/@me/add-friends/+page.server.ts
@@ -1,25 +1,29 @@
 import { error, redirect } from '@sveltejs/kit';
 
+export const load = async ({ locals: { supabase, user }, url }) => {
+	const { data } = await supabase.from('profile_invites').select('id').eq('user_id', user?.id);
+
+	if (data) return { invite_id: data[0].id, url_origin: url.origin };
+};
 
 export const actions = {
-    send_friend_request: async({ request, locals: {supabase, user} }) => {
+	send_friend_request: async ({ request, locals: { supabase, user } }) => {
+		if (!user) throw error(403, 'User is not signed in');
 
-        if(!user) throw error(403, "User is not signed in");
+		const data = await request.formData();
+		const receiver_username = data.get('receiver_username')?.toString();
+		const sender_username = data.get('sender_username')?.toString();
 
-        const data = await request.formData();
-        const receiver_username = data.get("receiver_username")?.toString();
-        const sender_username  = data.get("sender_username")?.toString();
+		const newConversationRequest = await supabase
+			.from('conversations')
+			.insert({
+				sender_username,
+				receiver_username
+			})
+			.select();
 
-        const newConversationRequest = await supabase
-            .from("conversations")
-            .insert({
-                sender_username, 
-                receiver_username
-            })
-            .select();
-        
-        if(!newConversationRequest.error){
-            throw redirect(300, `/channels/@me/${newConversationRequest.data[0].id}`);
-        }
-    }
-}
+		if (!newConversationRequest.error) {
+			throw redirect(300, `/channels/@me/${newConversationRequest.data[0].id}`);
+		}
+	}
+};
--- src/routes/(app)/channels/@me/add-friends/+page.svelte
+++ src/routes/(app)/channels/@me/add-friends/+page.svelte
@@ -1,29 +1,34 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { Avatar, getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
+	import { clipboard } from '@skeletonlabs/skeleton';
+	import { getToastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
 
 	export let data;
 
 	$: ({ username, supabase } = data);
+	const toastStore = getToastStore();
+
+	const toast: ToastSettings = {
+		message: 'Enlace de invitación copiado con éxito. ¡Invita a alguien a unirse a Messageeks!',
+		background: 'variant-filled-success'
+	};
+	const userInvite = `${data.url_origin}/invite/user/${data.invite_id}`;
 </script>
 
 <div class="card">
 	<section class="p-4 flex flex-col gap-20">
 		<div class="flex flex-col gap-1">
 			<div class="logo-cloud grid-cols-1 lg:!grid-cols-2 gap-0.5">
-				<a class="logo-item variant-filled-primary" href="/">
+				<button
+					class="logo-item variant-filled-primary"
+					on:click={() => toastStore.trigger(toast)}
+					use:clipboard={userInvite}
+				>
 					<span>
 						<span class="material-symbols-outlined text-2xl"> link </span>
 					</span>
 					<span>Enlace de invitación</span>
-				</a>
-				<a class="logo-item variant-filled-primary" href="/">
-					<span>
-						<span class="material-symbols-outlined text-2xl"> mail </span>
-					</span>
-					<span>Correo</span>
-				</a>
-
+				</button>
 				<!-- ... -->
 			</div>
 			<form action="?/send_friend_request" method="post">
