2ed0dbe9e20d4737e9ba704788ea7d670bffd393
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2024-08-13 20:38:23 -0600

    reset password funcionality implemented

--- src/routes/(auth)/auth/reset-password/+page.svelte
+++ src/routes/(auth)/auth/reset-password/+page.svelte
@@ -8,7 +8,7 @@
 	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
 		<img class="mx-auto h-20 w-auto" src={logo} alt="Messageeks" />
 		<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
-			Crea cuenta de Messageeks
+			Restablece tu contraseña
 		</h2>
 	</div>
 
@@ -22,7 +22,7 @@
 					</div>
 					<!-- Message -->
 					<div class="alert-message">
-						<h3 class="h3">{form.message}</h3>
+						<h3 class="h3">Hubo un error en la petición. Intenta de nuevo</h3>
 					</div>
 				</aside>
 			{/if}
@@ -34,8 +34,10 @@
 					</div>
 					<!-- Message -->
 					<div class="alert-message">
-						<h3 class="h3">El usuario ha sido creado con éxito</h3>
-						<p>Se ha enviado un correo a tu dirección de correo. Confirma tu correo electrónico</p>
+						<h3 class="h3">
+							La petición de restablecimiento de contraseña ha sido enviada con éxito
+						</h3>
+						<p>Comprueba en tu correo electrónico</p>
 					</div>
 				</aside>
 			{/if}
@@ -43,40 +45,11 @@
 				<label for="email" class="block text-sm font-medium leading-6">Correo electrónico</label>
 				<div class="mt-2">
 					<input
-						value={form?.email ?? ''}
 						id="email"
+						placeholder="Correo de la cuenta a recuperar..."
 						name="email"
 						type="email"
 						autocomplete="email"
-						required
-						class="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-					/>
-				</div>
-			</div>
-
-			<div>
-				<label for="password" class="block text-sm font-medium leading-6">Contraseña</label>
-				<div class="mt-2">
-					<input
-						id="password"
-						name="password"
-						type="password"
-						autocomplete="current-password"
-						required
-						class="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
-					/>
-				</div>
-			</div>
-			<div>
-				<label for="confirm-password" class="block text-sm font-medium leading-6"
-					>Confirmar contraseña</label
-				>
-				<div class="mt-2">
-					<input
-						id="confirm-password"
-						name="confirm-password"
-						type="password"
-						autocomplete="current-password"
 						required
 						class="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
 					/>
